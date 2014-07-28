e9c2bb041e2d99d8fa0f7f63611e0e13ed24ea9e
Mateo Hermida <mateohermida@outlook.com>
2014-07-28 00:34:47 -0300

    fixes sincronizacion y otros

--- app/src/main/java/com/ic/banking/glass/poc3_headgestures/HeadGestureDetector.java
+++ app/src/main/java/com/ic/banking/glass/poc3_headgestures/HeadGestureDetector.java
@@ -5,25 +5,40 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 import com.google.common.collect.EvictingQueue;
+import com.google.common.collect.Queues;
 
+import java.util.Iterator;
 import java.util.Queue;
 
 public class HeadGestureDetector {
 
+    private static final String TAG = HeadGestureDetector.class.getSimpleName();
+
+    private static final int ANGLES_QUEUE_SIZE = 100;
+
+    private static final Float NOD_LOW_ANGLE = new Float(-0.3);
+    private static final Float NOD_HIGH_ANGLE = new Float(0.3);
+    private static final Float HEAD_SHAKE_LOW_ANGLE = new Float(-1.0);
+    private static final Float HEAD_SHAKE_HIGH_ANGLE = new Float(1.0);
+
     private SensorManager sensorManager;
+
     private SensorEventListener sensorEventListener;
-
     private Context context;
-    private HeadGestureListener headGestureListener;
 
-    public static final int ANGLES_QUEUE_SIZE = 50;
-    private Queue<Float> nodAngles = EvictingQueue.create(ANGLES_QUEUE_SIZE);
-    private Queue<Float> headShakeAngles = EvictingQueue.create(ANGLES_QUEUE_SIZE);
+    private HeadGestureListener headGestureListener;
+    private Queue<Float> nodAngles = Queues.synchronizedQueue(EvictingQueue.<Float>create(ANGLES_QUEUE_SIZE));
+    private Queue<Float> headShakeAngles = Queues.synchronizedQueue(EvictingQueue.<Float>create(ANGLES_QUEUE_SIZE));
 
     private Thread checkThread;
     private boolean check;
+
+    private float angleA;
+    private float angleB;
+    private float angleNod;
 
     public HeadGestureDetector(Context context, HeadGestureListener headGestureListener) {
         this.context = context;
@@ -40,13 +55,18 @@ public class HeadGestureDetector {
             @Override
             public void run() {
                 while (check) {
+                    try {
+                        Thread.sleep(250);
+                        nodAngles.add(angleNod);
+                    }
+                    catch (InterruptedException e) {
+                    }
+
+                    Log.i(TAG, "AngleNod: " + angleNod);
                     checkIfNodOrHeadShake();
                 }
             }
         });
-
-        this.check = true;
-        this.checkThread.start();
     }
 
     public void stopListening() {
@@ -57,7 +77,7 @@ public class HeadGestureDetector {
         this.check = false;
     }
 
-    public void continueListening() {
+    public void startListening() {
         registerSensors();
 
         this.check = true;
@@ -73,7 +93,8 @@ public class HeadGestureDetector {
             @Override
             public void onSensorChanged(SensorEvent event) {
                 if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
-                    calculateNewAngles(event);
+                    test(event);
+                    //calculateNewAngles(event);
                 }
             }
         };
@@ -97,9 +118,11 @@ public class HeadGestureDetector {
 
     private void checkIfNodOrHeadShake() {
         boolean isNod = isNod();
-        boolean isHeadShake = isHeadShake();
+        boolean isHeadShake = false;//isHeadShake();
 
         if (isNod && !isHeadShake) {
+            logNodValues();
+            emptyNodAnglesQueue();
             this.headGestureListener.onNod();
         }
 
@@ -109,11 +132,33 @@ public class HeadGestureDetector {
     }
 
     private boolean isNod() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int lowPoints = 0;
+        int highPoints = 0;
+
+        for (Float nodAngle : nodAngles) {
+            if (nodAngle > NOD_HIGH_ANGLE) {
+                highPoints++;
+            }
+            if (nodAngle < NOD_LOW_ANGLE) {
+                lowPoints++;
+            }
+        }
+        return lowPoints >= 2 && highPoints >= 2;
     }
 
     private boolean isHeadShake() {
-        throw new UnsupportedOperationException("Not implemented yet");
+        int lowPoints = 0;
+        int highPoints = 0;
+
+        for (Float headShakeAngle : headShakeAngles) {
+            if (headShakeAngle > HEAD_SHAKE_HIGH_ANGLE) {
+                highPoints++;
+            }
+            if (headShakeAngle < HEAD_SHAKE_LOW_ANGLE) {
+                lowPoints++;
+            }
+        }
+        return lowPoints >= 2 && highPoints >= 2;
     }
 
     private Float calculateHeadShakeAngle(SensorEvent event) {
@@ -121,13 +166,51 @@ public class HeadGestureDetector {
     }
 
     private Float calculateNodAngle(SensorEvent event) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        float angle = (float) -Math.atan(event.values[2]
+                / Math.sqrt(event.values[1] * event.values[1] + event.values[0] * event.values[0]));
+
+        return angle;
     }
 
-    private Float computeOrientation(SensorEvent event) {
+    private Float computeOrientationA(SensorEvent event) {
         float angle = (float) -Math.atan(event.values[0]
                 / Math.sqrt(event.values[1] * event.values[1] + event.values[2] * event.values[2]));
 
         return angle;
+    }
+
+    private Float computeOrientationB(SensorEvent event) {
+        float angle = (float) -Math.atan(event.values[1]
+                / Math.sqrt(event.values[2] * event.values[2] + event.values[0] * event.values[0]));
+
+        return angle;
+    }
+
+    private void emptyNodAnglesQueue() {
+        synchronized (nodAngles) {
+            while (!nodAngles.isEmpty()) {
+                nodAngles.poll();
+            }
+        }
+    }
+
+    private void test(SensorEvent event) {
+        angleA = computeOrientationA(event);
+        angleB = computeOrientationB(event);
+        angleNod = calculateNodAngle(event);
+    }
+
+    private void logNodValues() {
+        synchronized (nodAngles) {
+            StringBuilder values = new StringBuilder();
+            Iterator<Float> i = nodAngles.iterator();
+            while (i.hasNext()) {
+                if (values.length() > 0) {
+                    values.append(';');
+                }
+                values.append(i.next());
+            }
+            Log.i(TAG, values.toString());
+        }
     }
 }
